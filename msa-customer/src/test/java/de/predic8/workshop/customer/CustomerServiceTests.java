93bdb3d26defc3ebff1601c0d19d7755420d1643
oliver <weiler@predic8.de>
2018-03-15 11:39:35 +0100

    Add JUnit 5

--- msa-customer/src/test/java/de/predic8/workshop/customer/CustomerServiceTests.java
+++ msa-customer/src/test/java/de/predic8/workshop/customer/CustomerServiceTests.java
@@ -6,9 +6,11 @@ import de.predic8.workshop.customer.service.CustomerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 import java.util.stream.Stream;
