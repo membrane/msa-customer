0af06834f68a59792c081ae8ffebf9ee45c57ad3
oliver <weiler@predic8.de>
2018-03-16 11:13:24 +0100

    Add Postgres backend

--- msa-customer/src/test/java/de/predic8/workshop/customer/CustomerRestControllerTests.java
+++ msa-customer/src/test/java/de/predic8/workshop/customer/CustomerRestControllerTests.java
@@ -17,7 +17,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@DisplayName("CustomerRestController")
+@DisplayName("A customer controller")
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(CustomerRestController.class)
 public class CustomerRestControllerTests {
